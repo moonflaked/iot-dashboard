d6fb6ceb67ba6692af362a0aa5ed3526ad2e94f0
Davidjorge2003 <89649485+Davidjorge2003@users.noreply.github.com>
2024-03-31 14:17:59 -0400

    Update DHT11.py

--- DHT11.py
+++ DHT11.py
@@ -1,24 +1,32 @@
-import time
 import Freenove_DHT as DHT
+import time
 
 class DHT11:
     def __init__(self, pin):
         self.DHTPin = pin
-        self.dht = DHT.DHT(pin)   # create a DHT class object
+        self.dht = DHT.DHT(pin)   # Create a DHT class object
 
-    def read_temperature_humidity(self):
+    def read_temperature(self):
         counts = 0  # Measurement counts
         while True:
             counts += 1
             for i in range(0, 15):            
-                chk = self.dht.readDHT11()     # read DHT11 and get a return value. Then determine whether data read is normal according to the return value.
-                if chk == self.dht.DHTLIB_OK:  # read DHT11 and get a return value. Then determine whether data read is normal according to the return value.
+                chk = self.dht.readDHT11()     # Read DHT11 and get a return value
+                if chk == self.dht.DHTLIB_OK:  # Check if data read is normal
+                    break
+                time.sleep(0.1)
+            temperature = self.dht.temperature
+            return temperature
+
+    def read_humidity(self):
+        counts = 0  # Measurement counts
+        while True:
+            counts += 1
+            for i in range(0, 15):            
+                chk = self.dht.readDHT11()     # Read DHT11 and get a return value
+                if chk == self.dht.DHTLIB_OK:  # Check if data read is normal
                     break
                 time.sleep(0.1)
             humidity = self.dht.humidity
-            temperature = self.dht.temperature
-            print("Humidity : %.2f, \t Temperature : %.2f \n" % (humidity, temperature))
-            return temperature, humidity
+            return humidity
 
-
-    
