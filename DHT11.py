764c5b2a4e8cf431cee7aa7ed45a09a325529ac7
Davidjorge2003 <89649485+Davidjorge2003@users.noreply.github.com>
2024-04-01 22:58:56 -0400

    Update DHT11.py

--- DHT11.py
+++ DHT11.py
@@ -4,27 +4,27 @@ import time
 class DHT11:
     def __init__(self, pin):
         self.DHTPin = pin
-        self.dht = DHT.DHT(pin)   # Create a DHT class object
+        self.dht = DHT.DHT(pin)   
 
     def read_temperature(self):
-        counts = 0  # Measurement counts
+        counts = 0  
         while True:
             counts += 1
             for i in range(0, 15):            
-                chk = self.dht.readDHT11()     # Read DHT11 and get a return value
-                if chk == self.dht.DHTLIB_OK:  # Check if data read is normal
+                chk = self.dht.readDHT11()     
+                if chk == self.dht.DHTLIB_OK:  
                     break
                 time.sleep(0.1)
             temperature = self.dht.temperature
             return temperature
 
     def read_humidity(self):
-        counts = 0  # Measurement counts
+        counts = 0  
         while True:
             counts += 1
             for i in range(0, 15):            
-                chk = self.dht.readDHT11()     # Read DHT11 and get a return value
-                if chk == self.dht.DHTLIB_OK:  # Check if data read is normal
+                chk = self.dht.readDHT11()     
+                if chk == self.dht.DHTLIB_OK:  
                     break
                 time.sleep(0.1)
             humidity = self.dht.humidity
