aefe3914a5de91373205628e8f3e96ef71b2267b
Davidjorge2003 <89649485+Davidjorge2003@users.noreply.github.com>
2024-04-01 23:01:43 -0400

    Update light_switch.py

--- light_switch.py
+++ light_switch.py
@@ -1,11 +1,10 @@
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-led_output_pin = 17
+
+GPIO.setmode(GPIO.BOARD)
+led_output_pin = 35  # This would be physical pin 35 that is the gpio19
 GPIO.setup(led_output_pin, GPIO.OUT)
 GPIO.output(led_output_pin, GPIO.LOW)
 
 def switch_state(led_state):
     GPIO.output(led_output_pin, led_state)
     return led_state
-    
-    
