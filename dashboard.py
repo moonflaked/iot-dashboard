e0e65aa6c9881fc5bfc7422afc6c4602e297013d
moonflaked <loganluo288@gmail.com>
2024-04-25 03:03:20 +0100

    Added new dashboard

--- dashboard.py
+++ dashboard.py
@@ -6,36 +6,195 @@ import dash_daq as daq
 import Freenove_DHT as DHT
 import email_send_receive as email_module
 import Motor as motor
-
+import dash_bootstrap_components as dbc
 motor.turn_off()
-dashboard_external_stylesheets = [
-    'dashboard.css'
-]
-app = Dash(__name__, external_stylesheets=dashboard_external_stylesheets)
 
-
-temperature_read_interval = dcc.Interval(
-    id="temperature-read-interval",
-    interval=2000,
-    n_intervals=0
+app = Dash(__name__,
+           external_stylesheets=[dbc.themes.GRID, 
+                                dbc.themes.BOOTSTRAP,
+                                 "https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
+                                 ],
 )
+iot_dashboard_title = dbc.Row(
+                        dbc.Col(
+                            html.Div(
+                                html.H1(
+                                    "IoT Dashboard",
+                                ),
+                                className="dashboard-title inter-title"
+                            )
+                        )
+                    )
 
-temperature_gauge = daq.Gauge(
-    id="temperature-gauge",
-    min=0,
-    max=50,
-    label="Temperature",
-    color={"gradient": True,
-           "ranges": {
-               "blue": [0, 15],
-               "green": [15, 25],
-               "yellow": [25, 35],
-               "red": [35, 50]
-           }
-           },
-    showCurrentValue=True,
-    units="°C"
-)
+temperature_card = dbc.Card(
+                    dbc.CardBody(
+                        [
+                            html.Div(
+                                html.H2(
+                                    "Temperature",    
+                                ),
+                                className="card-title default-border-color inter-card-title"
+                            ),
+                            html.Div(
+                                daq.Gauge(
+                                    id="temperature-gauge",
+                                    min=0,
+                                    max=50,
+                                    size=275,
+                                    label="        ",
+                                    color={"gradient": True,
+                                        "ranges": {
+                                            "blue": [0, 15],
+                                            "green": [15, 25],
+                                            "yellow": [25, 35],
+                                            "red": [35, 50]
+                                        }
+                                        },
+                                    showCurrentValue=True,
+                                    units="°C",
+                                    style={
+                                        "fontSize": "1rem"
+                                    }
+                                )
+                            )
+                        ],
+                        className="card-body default-border-color"
+                    ),
+                    className="border-0"
+                )
+
+humidity_card = dbc.Card(
+                    dbc.CardBody(
+                        [
+                            html.Div(
+                                html.H2(
+                                    "Humidity"
+                                ),
+                                className="card-title default-border-color inter-card-title"
+                            ),
+                            html.Div(
+                                daq.Gauge(
+                                    id="humidity-gauge",
+                                    min=20,
+                                    max=80,
+                                    size=275,
+                                    scale={
+                                        "custom": {
+                                            20: {"label": "20"},
+                                            30: {"label": "30"},
+                                            40: {"label": "40"},
+                                            50: {"label": "50"},
+                                            60: {"label": "60"},
+                                            70: {"label": "70"},
+                                            80: {"label": "80"},
+                                        }
+                                    },
+                                    label="     ",
+                                    color="#3c6bfa",
+                                    showCurrentValue=True,
+                                    units="%"
+                                )
+                            ),
+                        ],
+                        className="card-body default-border-color"
+                    ),
+                    className="border-0"
+                )
+light_intensity_card = dbc.Card(
+                        dbc.CardBody(
+                            [
+                                html.Div(
+                                    html.H2(
+                                        "Light Intensity"
+                                    ),
+                                    className="card-title default-border-color inter-card-title"
+                                ),
+                                html.Div(
+                                    [
+                                        html.Img(
+                                            src="assets/sun-icon.png",
+                                        ),
+                                        dbc.Progress(value=300, 
+                                                        max=1024, 
+                                                        style={"minHeight": "30px", "maxWidth": "80%", "fontSize": "2rem"},
+                                                        label="300",
+                                                        id="light-intensity-bar",
+                                                    className="intensity-bar")
+                                    ],
+                                    className="light-intensity-icon text-align-center"
+                                )
+                            ],
+                            className="card-body default-border-color"
+
+                        ),
+                        className="border-0"
+                    )
+
+light_status_card = dbc.Card(
+                        dbc.CardBody(
+                            [
+                                html.Div(
+                                    html.H2(
+                                        "Light Status"
+                                    ),
+                                    className="card-title default-border-color inter-card-title"
+                                ),
+                                html.Div(
+                                    [
+                                            html.Div(
+                                            children=[
+                                                html.Img(src="assets/closed-light.png", className="image-icon", id="light-icon-image")
+                                            ],
+                                            className="image-icon-div"
+                                        )
+                                    ],
+                                    className="text-align-center"
+                                )
+                            ],
+                            className="card-body default-border-color"
+
+                        ),
+                        className="border-0"
+                    )
+
+fan_status_card = dbc.Card(
+                    dbc.CardBody(
+                        [
+                            html.Div(
+                                html.H2(
+                                    "Fan Status"
+                                ),
+                                className="card-title default-border-color"
+                            ),
+                            html.Div(
+                                html.Div(
+                                    children=[
+                                        html.Img(
+                                            src="assets/fan.png",
+                                            id="fan-icon-image"
+                                        )
+                                    ],
+                                    className="image-icon-div"
+                                ),
+                                className="text-align-center"
+                            )
+                        ],
+                        className="card-body default-border-color"
+                    ),
+                    className="border-0"
+                )
+
+email_sent_toast = dbc.Toast(
+                        "An email has been sent!",
+                        id="positioned-toast",
+                        header="Light Intensity Threshold Reached",
+                        is_open=True,
+                        dismissable=True,
+                        icon="info",
+                        duration=4000,
+                        # top: 66 positions the toast below the navbar
+                        style={"position": "fixed", "width": 220, "height": 70},
+                    )
 
 humidity_read_interval = dcc.Interval(
     id="humidity-read-interval",
@@ -43,94 +202,134 @@ humidity_read_interval = dcc.Interval(
     n_intervals=0
 )
 
-
-
-humidity_gauge = daq.Gauge(
-    id="humidity-gauge",
-    min=20,
-    max=80,
-    scale={
-        "custom": {
-            20: {"label": "20"},
-            30: {"label": "30"},
-            40: {"label": "40"},
-            50: {"label": "50"},
-            60: {"label": "60"},
-            70: {"label": "70"},
-            80: {"label": "80"},
-        }
-    },
-    label="Humidity",
-    color="#3c6bfa",
-    showCurrentValue=True,
-    units="%",
+temperature_read_interval = dcc.Interval(
+    id="temperature-read-interval",
+    interval=2000,
+    n_intervals=0
 )
 
-
-app.layout = html.Div([
-    html.Div(
-        className="displayInlineBlock verticalAlignTop",
-        children=[
-            html.Div(
-                children=[
-                    html.Button(children=[
-                        html.Img(src="assets/off-button.png", className="image-icon", id="button-image")
-                    ],
-                        id="light-button",
-                        n_clicks=0),
-                ]
-            ),
-            html.Div(
-                children=[
-                    html.Img(src="assets/closed-light.png", className="image-icon", id="light-icon-image")
-                ],
-                id="light-icon"
-            ),
-        ]
-    ),
-    html.Div(
-        className="displayInlineBlock verticalAlignTop",
-        children=[
-            temperature_gauge,
-            humidity_gauge,
-            temperature_read_interval,
-            humidity_read_interval
-        ]
-    ),
-    html.Div(
-        className="displayInlineBlock",
-        children=[
-            html.Div(
-                children=[
-                    html.Img(
-                        src="assets/fan.png",
-                        id="fan-icon-image"
-                    )
-                ]
-            )
-        ]
-    )
-])
-
-
-@callback(
+app.layout = dbc.Container(
     [
-        Output(component_id="button-image", component_property="src"),
-        Output(component_id="light-icon-image", component_property="src"),
-    ],
-    Input(component_id="light-button", component_property="n_clicks")
-)
-def change_light_state(n_clicks):
-    # Toggles light state from 0 or 1 indicating whether light is open or closed
-    # The real LED state from the output of the LED pin determines the light state on the dashboard
-    # because we do not want to show a "fake state" to the user
-    calculated_led_state = n_clicks % 2
-    real_led_state = light.switch_state(calculated_led_state)
+        dbc.Row(
+            dbc.Col(
+                html.Div(
+                    [
+                        iot_dashboard_title
+                    ]
+                ),
+                className="g-0"
+            )
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        dbc.Row(
+                            [
+                                dbc.Col(
+                                    temperature_card,
+                                    className="dashboard-card-left dashboard-card",
+                                    width={"size":6}
+                                ),
+                                dbc.Col(
+                                    humidity_card,
+                                    className="dashboard-card-right dashboard-card",
+                                    width={"size":6}
+                                    
+                                )
+                            ],
+                            
+                        ),
+                        dbc.Row(
+                            [
+                                dbc.Col(
+                                    html.Div(
+                                        [
+                                            light_intensity_card
+                                        ],
+                                    ),
+                                    className="dashboard-card-left dashboard-card ",
+                                    width=6
+                                ),
+                                dbc.Col(
+                                    html.Div(
+                                        [
+                                            light_status_card
+                                        ],
+                                        
+                                    ),
+                                    className="dashboard-card-right ",
+                                    width=6
+                                )
+                            ]
+                        ),
+                        dbc.Row(
+                            [
+                                dbc.Col(
+                                    html.Div(
+                                        [
+                                            fan_status_card
+                                        ],
+                                        
+                                    ),
+                                    className="dashboard-card-left",
+                                    width=6
+                                ),
+                                dbc.Col(
+                                    html.Div(
+                                        [
 
-    if real_led_state:
-        return "assets/on-button.png", "assets/open-light.png"
-    else:
-        return "assets/off-button.png", "assets/closed-light.png"
+                                        ],
+                                        #className="dev-color-4"
+                                    ),
+                                    className="dashboard-card-right",
+                                    width=6
+                                )
+                            ]
+                        ),
+                    ],
+                    width=8,
+                    #className="dev-color-1"
+                ),
+                dbc.Col(
+                    html.Div(
+                        [
+
+                        ],
+                       # className="dev-color-2"
+                    ),
+                    width=4
+                ),
+            ],
+            className="card-container"
+        ),
+        email_sent_toast,
+        temperature_read_interval,
+        humidity_read_interval
+    ],
+    className="g-0",
+    fluid=True
+)
+
+
+# @callback(
+#     [
+#         Output(component_id="button-image", component_property="src"),
+#         Output(component_id="light-icon-image", component_property="src"),
+#     ],
+#     Input(component_id="light-button", component_property="n_clicks")
+# )
+# def change_light_state(n_clicks):
+#     # Toggles light state from 0 or 1 indicating whether light is open or closed
+#     # The real LED state from the output of the LED pin determines the light state on the dashboard
+#     # because we do not want to show a "fake state" to the user
+#     calculated_led_state = n_clicks % 2
+#     real_led_state = light.switch_state(calculated_led_state)
+
+#     if real_led_state:
+#         return "assets/on-button.png", "assets/open-light.png"
+#     else:
+#         return "assets/off-button.png", "assets/closed-light.png"
 
 @callback(
     Output(component_id="temperature-gauge", component_property="value"),
